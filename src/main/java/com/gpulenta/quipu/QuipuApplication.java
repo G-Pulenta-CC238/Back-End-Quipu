07a9327f479544ee1df3f5bd05681f259f67cb57
u201817461 <u201817461@upc.edu.pe>
2023-09-12 22:05:16 -0500

    Add: Project Base

---
+++ src/main/java/com/gpulenta/quipu/QuipuApplication.java
@@ -0,0 +1,13 @@
+package com.gpulenta.quipu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpulenta/quipu/QuipuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpulenta.quipu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuipuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
